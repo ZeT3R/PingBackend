47dca73d8af1fe147bd91c8294bf90e45963bf5e
ZeT3R <yaros23lav23@gmail.com>
2024-10-12 15:38:16 +0300

    Spring project initialize

---
+++ pinglocal/src/main/java/com/ping/pinglocal/PinglocalApplication.java
@@ -0,0 +1,13 @@
+package com.ping.pinglocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinglocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinglocalApplication.class, args);
+	}
+
+}
